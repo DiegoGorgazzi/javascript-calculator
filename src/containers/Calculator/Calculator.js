f5d5ad21bd1970a4a01c209896da8c1b2f3d728c
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-04-17 14:31:56 +0200

    Add some logic to resultClickHandler

--- src/containers/Calculator/Calculator.js
+++ src/containers/Calculator/Calculator.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import CalculatorControl from "../../components/CalculatorControl/CalculatorControl";
 
 class Calculator extends Component {
+    state = {
+        result: 0
+    }
 
-  clickHandler = (event) => {
-      console.log("yay clicked")
+
+  resultClickHandler = (action, value) => {
+      switch (action) {
+        case "plus":
+            this.setState( ( prevState ) => { return { result: prevState.result + 1 } } )
+            break;
+        case "minus":
+            this.setState( ( prevState ) => { return { result: prevState.result - 1 } } )
+            break;
+
+      }
+
     }
 
     render () {
         return (
             <div>
-              <CalculatorControl id="add" label="+" clicked={this.clickHandler} />
+              <CalculatorControl id="add" label="+" clicked={() => this.resultClickHandler( 'plus' )} />
+              <CalculatorControl id="subtract" label="-" clicked={() => this.resultClickHandler( 'minus' )} />
 
             </div>
         );
