ed125cfceedb1b30ba96df3e8b96fe21a579728f
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-04-18 15:43:43 +0200

    Add clearHandler and more operators

--- src/containers/Calculator/Calculator.js
+++ src/containers/Calculator/Calculator.js
@@ -5,8 +5,12 @@ import CalculatorNumbers from "../../components/CalculatorNumbers/CalculatorNumb
 
 class Calculator extends Component {
     state = {
-        result: 0
+        result: 0,
+        currentInput: 0
     }
+
+
+
 
 
   resultClickHandler = (action, value) => {
@@ -17,20 +21,43 @@ class Calculator extends Component {
         case "minus":
             this.setState( ( prevState ) => { return { result: prevState.result - 1 } } )
             break;
-
       }
-
     }
 
+  numberClick = (event) => {
+    let userInput = event.target.value;
+    this.setState( ( prevState ) => {
+      return { currentInput: prevState.currentInput + userInput }
+    });
+    console.log(this.state.currentInput, "current");
+  }
+
+  clearHandler = () => {
+    this.setState({
+      currentInput: 0
+    });
+  }
+
     render () {
+      console.log(this.state.currentInput, "current Outside");
         return (
             <div>
+              <div id="display">
+                  {this.state.currentInput}
+              </div>
+
               <CalculatorOutput value={this.state.result} />
 
-              <CalculatorControl id="add" label="+" clicked={() => this.resultClickHandler( 'plus' )} />
-              <CalculatorControl id="subtract" label="-" clicked={() => this.resultClickHandler( 'minus' )} />
+              <CalculatorControl myID="add" label="+" clicked={() => this.resultClickHandler( 'plus' )} />
+              <CalculatorControl myID="subtract" label="-" clicked={() => this.resultClickHandler( 'minus' )} />
+              <CalculatorControl myID="multiply" label="x" clicked={this.numberClick} />
+              <CalculatorControl myID="divide" label="/" clicked={this.numberClick} />
+              <CalculatorControl myID="equals" label="=" clicked={() => this.resultClickHandler( 'minus' )} />
+              <CalculatorControl myID="clear" label="CLEAR" clicked={this.clearHandler} />
+              <br />
+              <CalculatorNumbers numClick={this.numberClick} />
+              <br />
 
-              <CalculatorNumbers />
 
             </div>
         );
