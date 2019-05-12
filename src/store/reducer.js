9b482b7c457a3e210c17321c7befd147c7426704
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-05-12 12:38:27 +0200

    Create reducer and import createStore from Redux

---
+++ src/store/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+      result: "0",
+      currentInput: "0",
+      operatorDisabled: false,
+      zeroDisabled: false ,
+      decimalDisabled: false,
+      numbersDisabled: false,
+      equalPressed: false,
+      equalDisabled: false
+}
+
+const reducer = (state = initialState, action) => {
+    return state;
+};
+
+export default reducer;
